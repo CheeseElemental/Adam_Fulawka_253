b7e6072208ef205a5bf064a7145a79a2c2009c74
CheeseElemental <fulawka.adam@gmail.com>
2023-09-07 15:37:42 -0400

    Project_01
    A Yellow square made by Adam Fulawka

---
+++ Project_01/js/script.js
@@ -0,0 +1,34 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(480, 480);
+    background(255, 255, 0);
+
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
